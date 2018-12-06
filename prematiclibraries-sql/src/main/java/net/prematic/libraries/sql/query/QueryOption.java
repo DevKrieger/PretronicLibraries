58d2e16718a89cf3b438866831a568ed34001bd6
Fridious <philippfriedhoff@gmx.de>
2018-12-06 19:29:42 +0100

    renamed Type to ColumnType, renamed QueryOptions to QueryOption

---
+++ prematiclibraries-sql/src/main/java/net/prematic/libraries/sql/query/QueryOption.java
@@ -0,0 +1,13 @@
+package net.prematic.libraries.sql.query;
+
+/*
+ *
+ *  * Copyright (c) 2018 Davide Wietlisbach on 19.09.18 08:01
+ *
+ */
+
+public class QueryOption {
+
+    public static final String NOT_NULL = "NOT NULL";
+    public static final String UNIQUE = "UNIQUE";
+}
