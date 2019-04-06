bcf15d40de008ad39fa8e132d308c16864793300
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2019-04-06 17:09:04 +0200

    Started java reflect utility  - Created unsafe instance creator (Without constructor)  - Created Primitive list  - Created field and method reflection

---
+++ prematiclibraries-utility/src/main/java/net/prematic/libraries/utility/reflect/ReflectionUtil.java
@@ -0,0 +1,86 @@
+/*
+ * (C) Copyright 2019 The PrematicLibraries Project (Davide Wietlisbach & Philipp Elvin Friedhoff)
+ *
+ * @author Davide Wietlisbach
+ * @since 17.02.19 14:38
+ *
+ * The PrematicLibraries Project is under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package net.prematic.libraries.utility.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectionUtil {
+
+    public static Field getField(Class<?> clazz, String fieldName){
+        try {
+            return clazz.getField(fieldName);
+        } catch (Exception exception) {throw new ReflectException(exception);}
+    }
+
+    public static void changeFieldValue(Class<?> clazz,String fieldName,Object value){
+        changeFieldValue(clazz,null, fieldName, value);
+    }
+
+    public static void changeFieldValue(Object object,String fieldName,Object value){
+        changeFieldValue(object.getClass(),object, fieldName, value);
+    }
+
+    public static void changeFieldValue(Class<?> clazz, Object object,String fieldName,Object value){
+        try {
+            Field field = getField(clazz,fieldName);
+            if(field != null){
+                field.setAccessible(true);
+                field.set(object,value);
+            }
+        } catch (Exception exception) {throw new ReflectException("Could not change file "+fieldName+" from class "+clazz,exception);}
+    }
+
+    public static Method getMethod(Class<?> clazz, String methodName){
+        try {
+            return clazz.getMethod(methodName);
+        } catch (Exception exception) {throw new ReflectException(exception);}
+    }
+
+    public static Method getMethod(Class<?> clazz, String methodName,Class<?>[] parameters){
+        try {
+            return clazz.getMethod(methodName,parameters);
+        } catch (Exception exception) {throw new ReflectException(exception);}
+    }
+
+    public static Object invokeMethod(Object object, String methodName, Object...parameters){
+        return invokeMethod(object.getClass(),object, methodName,parameters);
+    }
+
+    public static Object invokeMethod(Class<?> clazz, Object object, String methodName, Object[] parameters){
+        try {
+            Method method = getMethod(clazz,methodName,buildTyeArray(parameters));
+            method.setAccessible(true);
+            return method.invoke(object,parameters);
+        }  catch (Exception exception) {throw new ReflectException("Could not invoke method  "+methodName+" from class "+object.getClass(),exception);}
+    }
+
+    public static Object invokeMethod(Method method, Object object, Object parameters){
+        try {
+            return method.invoke(object,parameters);
+        }  catch (Exception exception) {throw new ReflectException("Could not invoke method  "+method.getName()+" from class "+object.getClass());}
+    }
+
+    public static Class<?>[] buildTyeArray(Object[] array){
+        Class<?>[] types = new Class<?>[array.length];
+        for(int i = 0;i <array.length;i++) if(array[i] != null) types[i] = array[i].getClass();
+        return types;
+    }
+}
---
+++ prematiclibraries-utility/src/main/java/net/prematic/libraries/utility/reflect/UnsafeInstanceCreator.java
@@ -0,0 +1,94 @@
+/*
+ * (C) Copyright 2019 The PrematicLibraries Project (Davide Wietlisbach & Philipp Elvin Friedhoff)
+ *
+ * @author Davide Wietlisbach
+ * @since 06.04.19 17:08
+ *
+ * The PrematicLibraries Project is under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package net.prematic.libraries.utility.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public interface UnsafeInstanceCreator {
+
+    UnsafeInstanceCreator DEFAULT = findCreator();
+
+    <T> T createInstance(Class<T> clazz) throws Exception;
+
+    static <T> T newInstance(Class<T> clazz) throws Exception{
+        System.out.println("create instance for "+clazz);
+        return DEFAULT.createInstance(clazz);
+    }
+
+    static UnsafeInstanceCreator findCreator(){
+
+        try {//Sun Unsafe
+            Class<?> clazz = Class.forName("sun.misc.Unsafe");
+            Field field = clazz.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+            Method allocator = clazz.getMethod("allocateInstance",Class.class);
+            return new UnsafeInstanceCreator(){
+                @Override
+                public <T> T createInstance(Class<T> clazz) throws Exception{
+                    return (T) allocator.invoke(unsafe,clazz);
+                }
+            };
+        } catch (Exception ignored) {}
+
+        try {//Java io ObjectStreamClass
+            Class<?> clazz = Class.forName("java.io.ObjectStreamClass");
+            Method constructor = clazz.getDeclaredMethod("getConstructorId", Class.class);
+            constructor.setAccessible(true);
+            int constructorId = (Integer) constructor.invoke(null, Object.class);
+            Method creator = clazz.getDeclaredMethod("newInstance", Class.class, int.class);
+            creator.setAccessible(true);
+            return new UnsafeInstanceCreator(){
+                @Override
+                public <T> T createInstance(Class<T> clazz) throws Exception {
+                    checkInstanceAble(clazz);
+                    return (T) creator.invoke(null, clazz, constructorId);
+                }
+            };
+        } catch (Exception ignored) {}
+
+        try {//Java io ObjectInputStream
+            final Method creator = Class.forName("java.io.ObjectInputStream").getDeclaredMethod("newInstance", Class.class, Class.class);
+            creator.setAccessible(true);
+            return new UnsafeInstanceCreator() {
+                @Override
+                public <T> T createInstance(Class<T> clazz) throws Exception {
+                    checkInstanceAble(clazz);
+                    return (T) creator.invoke(null, clazz, Object.class);
+                }
+            };
+        } catch (Exception ignored) {}
+
+        return new UnsafeInstanceCreator() {
+            @Override
+            public <T> T createInstance(Class<T> clazz) {
+                throw new IllegalArgumentException("No unsafe instance creator found.");
+            }
+        };
+    }
+
+    static void checkInstanceAble(Class<?> clazz){
+        if (Modifier.isAbstract(clazz.getModifiers()) || Modifier.isInterface(clazz.getModifiers())) {
+            throw new IllegalArgumentException("Can not create a instance of an interface or an abstract class ("+clazz.getName()+")");
+        }
+    }
+}
