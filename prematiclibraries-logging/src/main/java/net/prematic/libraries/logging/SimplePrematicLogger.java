dfe169ebf10756d992e253ed6e5df66c4d8cf255
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2019-06-08 17:38:00 +0200

    Created SLF4j static logger adapter for version 1.7 and 1.8
    Took 3 hours 48 minutes

--- prematiclibraries-logging/src/main/java/net/prematic/libraries/logging/SimplePrematicLogger.java
+++ prematiclibraries-logging/src/main/java/net/prematic/libraries/logging/SimplePrematicLogger.java
@@ -69,6 +69,7 @@ public class SimplePrematicLogger extends AbstractPrematicLogger {
         this.queue = new LinkedBlockingQueue<>();
         this.printer = new AsyncQueuePrinter(this.queue);
         this.printer.start();
+        info("Staring Prematic logging service.");
     }
 
     public void formatAndWrite(MessageInfo info, LogLevel logLevel, DebugLevel debugLevel, String message, Throwable throwable, Thread thread){
