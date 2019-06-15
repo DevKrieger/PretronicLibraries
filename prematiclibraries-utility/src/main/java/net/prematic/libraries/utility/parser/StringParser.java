d010ab2bc2cf9f80a9e62efcef03d0a4f62dd44e
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2019-06-15 20:20:34 +0200

    Created StringParser utility for parsing text file types
    Took 2 minutes

---
+++ prematiclibraries-utility/src/main/java/net/prematic/libraries/utility/parser/StringParser.java
@@ -0,0 +1,502 @@
+/*
+ * (C) Copyright 2019 The PrematicWeb Project (Davide Wietlisbach & Philipp Elvin Friedhoff)
+ *
+ * @author Davide Wietlisbach
+ * @since 25.04.19 09:44
+ *
+ * The PrematicWeb Project is under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package net.prematic.libraries.utility.parser;
+
+import net.prematic.libraries.utility.io.FileUtil;
+import net.prematic.libraries.utility.io.IORuntimeException;
+import net.prematic.libraries.utility.map.Pair;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class StringParser {
+
+    private final char[][] lines;
+    private int lineIndex, charIndex;
+
+    //Constructor
+
+    public StringParser(File location) {
+        this(FileUtil.newFileInputStream(location));
+    }
+
+    public StringParser(File location, Charset charset) {
+        this(FileUtil.newFileInputStream(location),charset);
+    }
+
+    public StringParser(InputStream stream){
+        this(new BufferedReader(new InputStreamReader(stream)));
+    }
+
+    public StringParser(InputStream stream, Charset charset) {
+        this(new BufferedReader(new InputStreamReader(stream,charset)));
+    }
+
+    public StringParser(BufferedReader reader)  {
+        try{
+            ArrayList<char[]> lines = new ArrayList<>();
+
+            //Read lines from reader
+            String line;
+            while((line = reader.readLine())!= null) lines.add(line.toCharArray());
+
+            this.lines = lines.toArray(new char[0][]);
+            resetIndex();
+        }catch (IOException exception){
+            throw new IORuntimeException(exception);
+        }
+    }
+
+    public StringParser(String content){
+        String[] rawLines = content.split("\n");
+
+        this.lines = new char[rawLines.length][];
+        for(int i = 0;i<rawLines.length;i++) this.lines[i] = rawLines[i].toCharArray();
+        resetIndex();
+    }
+
+    public StringParser(char[][] lines) {
+        this.lines = lines;
+        resetIndex();
+    }
+
+
+
+    //Index operations
+
+    public int lineIndex(){
+        return this.lineIndex;
+    }
+
+    public int charIndex(){
+        return this.charIndex;
+    }
+
+    public void setCharIndex(int charIndex){
+        this.charIndex = charIndex;
+    }
+
+    public void setLineIndex(int lineIndex){
+        this.lineIndex = lineIndex;
+    }
+
+    public void setIndex(int lineIndex, int charIndex){
+        this.lineIndex = lineIndex;
+        this.charIndex = charIndex;
+    }
+
+    public void resetIndex(){
+        this.lineIndex = 0;
+        this.charIndex = 0;
+    }
+
+    //Check
+    public void setPosition(int position){
+        int newLineIndex = 0;
+        while(position > this.lines[newLineIndex].length){
+            position -= this.lines[newLineIndex].length;
+            newLineIndex++;
+        }
+        setIndex(newLineIndex,position);
+    }
+
+
+
+    //Index Checks (Check all)
+
+    public boolean hasNext(){
+        return hasNextChar();
+    }
+
+    public boolean hasNextChar(){
+        return hasChar(this.lineIndex,this.charIndex);
+    }
+
+    public boolean hasNextLine(){
+        return hasLine(this.lineIndex);
+    }
+
+    public boolean has(int lineIndex, int charIndex){
+        return hasChar(lineIndex,charIndex) || hasLine(lineIndex);
+    }
+
+    public boolean hasChar(int lineIndex,int charIndex){
+        return lines.length>lineIndex && lines[lineIndex].length > charIndex;
+    }
+
+    public boolean hasLine(int lineIndex){
+        return lines.length>lineIndex;
+    }
+
+
+
+    //Line operation
+
+    public char[][] getLines(){
+        return this.lines;
+    }
+
+    public String[] lines(){
+        String[] lines = new String[this.lines.length];
+        StringBuilder lineBuilder = new StringBuilder();
+        for(int i = 0;i<lines.length;i++){
+            for(char c : this.lines[i])lineBuilder.append(c);
+            lines[i] = lineBuilder.toString();
+            lineBuilder.setLength(0);
+        }
+        return lines;
+    }
+
+    public String[] lines(int from, int to){
+        return null;
+    }
+
+    public String line(){
+        return line(this.lineIndex);
+    }
+
+    public String line(int lineIndex){
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0;i<this.lines[lineIndex].length;i++) builder.append(this.lines[lineIndex][i]);
+        return builder.toString();
+    }
+
+    public String nextLine(){
+        String result =  line();
+        this.lineIndex++;
+        this.charIndex = 0;
+        return result;
+    }
+
+    public void skipLine(){
+        skipLines(1);
+    }
+
+    public void skipLines(int amount){
+        this.lineIndex += amount;
+        this.charIndex = 0;
+    }
+
+    public void previousLine(){
+        previousLine(1);
+    }
+
+    public void previousLine(int amount){
+        this.lineIndex = this.lineIndex-amount;
+        if(hasLine(this.lineIndex)) this.charIndex = this.lines[lineIndex].length-1;
+        else this.charIndex = 0;
+    }
+
+
+
+    //Char operation
+
+    public char currentChar(){
+        return charAt(this.charIndex);
+    }
+
+    public char charAt(int index){
+        return charAt(this.lineIndex,index);
+    }
+
+    public char charAt(int lineIndex, int charIndex){
+        return this.lines[lineIndex][charIndex];
+    }
+
+    public void skipChar(){
+        skipChars(1);
+    }
+
+    public char nextChar(){
+        char result = this.lines[this.lineIndex][this.charIndex];
+        if(this.lines[this.lineIndex].length > charIndex+1) charIndex++;
+        else{
+            lineIndex++;
+            charIndex = 0;
+        }
+        return result;
+    }
+
+    public void skipChars(int amount){
+        if(this.lineIndex < 0) skipLine();
+        int newLineIndex = this.lineIndex;
+        int newCharIndex = this.charIndex+amount;
+
+        while(newCharIndex >= lines[newLineIndex].length){
+            newLineIndex++;
+            newCharIndex = newCharIndex-lines[newLineIndex-1].length;
+        }
+        setIndex(newLineIndex,newCharIndex);
+    }
+
+    public void skipChars(char c){
+        skipChars(c,null);
+    }
+
+    public void skipChars(char c, Character comment){
+        nextCharIndex(c, comment);
+    }
+
+    public void previousChar(){
+        previousChars(1);
+    }
+
+    public void previousChars(int amount){
+        int newLineIndex = this.lineIndex;
+        int newCharIndex = this.charIndex-amount;
+
+        while(newCharIndex < -1 && newLineIndex > 0){
+            newLineIndex--;
+            newCharIndex = lines[newLineIndex].length+newCharIndex;
+        }
+        setIndex(newLineIndex,newCharIndex);
+    }
+
+    public Pair<Integer,Integer> getEnd(){
+        int lastLine = this.lineIndex = this.lines.length-1;
+        return new Pair<>(lastLine,this.lines[lastLine].length);
+    }
+
+    public void end(){
+        this.lineIndex = this.lines.length-1;
+        this.charIndex = this.lines[this.lineIndex].length;
+    }
+
+    //Search options
+
+    public Pair<Integer,Integer> findNextCharIndex(char c){
+        return findNextCharIndex(c,(Character)null);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(char c, Character comment){
+        return findNextCharIndex(c,null, comment);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(char c, Consumer<Character> validator){
+        return findNextCharIndex(c,validator, null);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(char c, Consumer<Character> validator, Character comment){
+        return findNextCharIndex(this.lineIndex,this.charIndex,c,validator, comment);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(int lineIndex, int charIndex, char c, Character comment){
+        return findNextCharIndex(lineIndex, charIndex, c,null,comment);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(int lineIndex, int charIndex, char c, Consumer<Character> validator){
+        return findNextCharIndex(lineIndex, charIndex, c,validator,null);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(int lineIndex, int charIndex, char c, Consumer<Character> validator, Character comment){
+        char previous = ' ';
+        while(has(lineIndex,charIndex)){
+            char current = charAt(lineIndex,charIndex);
+            if(current == c && (comment == null || previous != comment)) return new Pair<>(lineIndex,charIndex);
+            else if(validator != null) validator.accept(current);
+            previous = current;
+            if(this.lines[lineIndex].length > charIndex+1) charIndex++;
+            else{
+                lineIndex++;
+                charIndex = 0;
+            }
+        }
+        return null;
+    }
+
+
+    public Pair<Integer,Integer> findNextCharIndex(Function<Character,Boolean> acceptor){
+        return findNextCharIndex(this.lineIndex,this.charIndex,acceptor);
+    }
+
+    public Pair<Integer,Integer> findNextCharIndex(int lineIndex, int charIndex, Function<Character,Boolean> acceptor){
+        while(has(lineIndex,charIndex)){
+            char current = charAt(lineIndex,charIndex);
+            if(acceptor.apply(current)) return new Pair<>(lineIndex,charIndex);
+            if(this.lines[lineIndex].length > charIndex+1) charIndex++;
+            else{
+                lineIndex++;
+                charIndex = 0;
+            }
+        }
+        return null;
+    }
+
+
+    public Pair<Integer,Integer> nextCharIndex(char c, Character comment){
+        char previous = ' ';
+        while(hasNext()){
+            if(nextChar() == c && (comment == null || previous != comment)) return new Pair<>(this.lineIndex,this.charIndex);
+            previous = currentChar();
+        }
+        return null;
+    }
+
+
+    //Get Part operations
+
+    public String get(int line, int from, int to){
+        return String.copyValueOf(Arrays.copyOfRange(lines[line],from,to));
+    }
+
+    public String get(int toLine, int toChar){
+        return get(this.lineIndex,this.charIndex,toLine,toChar);
+    }
+
+    public String get(int fromLine, int fromChar, int toLine, int toChar){
+        if(fromLine == toLine) return get(fromLine,fromChar,toChar);
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = fromLine;i<=toLine;i++){
+            for(int e = (i==fromLine?fromChar:0);e<this.lines[i].length;e++){
+                if(i == toLine){
+                    if(e >= toChar) break;
+                    else builder.append(this.lines[i][e]);
+                }else builder.append(this.lines[i][e]);
+            }
+            if(i != toLine) builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    public String getUntil(int lineIndex, int charIndex, char c,Consumer<Character> validator, Character comment){
+        Pair<Integer,Integer> index = findNextCharIndex(lineIndex,charIndex,c,validator, comment);
+        return get(index.getKey(),index.getValue());
+    }
+
+    public String nextUntil(char c){
+        return nextUntil(c,null,null,false);
+    }
+
+    public String nextUntil(char c, Consumer<Character> validator){
+        return nextUntil(c,validator,null,false);
+    }
+
+    public String nextUntil(char c, Character comment){
+        return nextUntil(c,null,comment,false);
+    }
+
+    public String nextUntil(char c, Consumer<Character> validator, Character comment, boolean nullByEnd){
+        return nextUntil(findNextCharIndex(c,validator, comment));
+    }
+
+    public String nextUntil(Function<Character,Boolean> acceptor){
+        return nextUntil(acceptor,false);
+    }
+
+    public String nextUntil(Function<Character,Boolean> acceptor, boolean nullByEnd){
+        return nextUntil(findNextCharIndex(acceptor),nullByEnd);
+    }
+
+    public String nextUntil(Pair<Integer,Integer> index){
+        return nextUntil(index,false);
+    }
+
+    public String nextUntil(Pair<Integer,Integer> index, boolean nullByEnd){
+        String result;
+        if(index == null) {
+            if(nullByEnd) return null;
+            else{
+                index = getEnd();
+                result = get(index.getKey(),index.getValue());
+                end();
+            }
+        }else result = get(index.getKey(),index.getValue());
+        setIndex(index.getKey(),index.getValue());
+        return result;
+    }
+
+    //Copy operations
+
+    public StringParser copyOf(int fromLine, int fromIndex){
+        return copyOf(fromLine, fromIndex,this.lineIndex,this.charIndex);
+    }
+
+    public StringParser copyOf(int fromLine, int fromIndex,int toLine, int toIndex){
+        if(fromIndex > this.lines[fromLine].length) fromLine++;
+
+        char[][] lines = Arrays.copyOfRange(this.lines,fromLine,toLine+1);
+
+        char[] first = lines[0];
+        if(fromIndex > 0 && fromIndex < first.length){
+            lines[0] = Arrays.copyOfRange(first,fromIndex,lines[0].length);
+        }
+
+        char[] last = lines[lines.length-1];
+
+        if(toIndex < last.length) lines[lines.length-1] = Arrays.copyOfRange(last,0,toIndex+1);
+        return new StringParser(lines);
+    }
+
+    //Parser exception
+
+    public String getExceptionContent(int line, int index){
+        return getExceptionContent(line, index,null);
+    }
+
+    public String getExceptionContent(int line, int index, String prefix){
+        StringBuilder builder = new StringBuilder();
+        buildExceptionContent(line,index,builder,prefix);
+        return builder.toString();
+    }
+
+    public void buildExceptionContent(int line, int index, StringBuilder builder){
+        buildExceptionContent(line,index,builder,null);
+    }
+
+    public void buildExceptionContent(int line, int index, StringBuilder builder, String prefix){
+        int start = index>10?index-18:0;
+        int end = index+18;
+
+        while(end > lines[line].length) end--;
+
+        if(prefix != null) builder.append(prefix);
+        builder.append(get(line,start,end));
+        builder.append(System.lineSeparator());
+        if(prefix != null) builder.append(prefix);
+        for(int i = 0;i<index-start;i++) builder.append(" ");
+        builder.append("^");
+    }
+
+    public void throwException(String message){
+        throw new ParserException(this,lineIndex,charIndex,"Exception at "+(lineIndex+1)+":"+(charIndex+1)+" -> "+message);
+    }
+
+    public void throwException(String message, Exception exception){
+        throw new ParserException(this,lineIndex,charIndex,message,exception);
+    }
+
+
+
+    //Object operations
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0;i<lines.length;i++) builder.append(lines[i]).append('\n');
+        builder.setLength(builder.length()-1);
+        return builder.toString();
+    }
+}
