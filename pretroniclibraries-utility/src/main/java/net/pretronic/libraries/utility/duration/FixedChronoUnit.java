9e8b2a27aba4695490ad9eaf2db1c5327acbbbfa
Fridious <philippfriedhoff@outlook.de>
2021-08-06 20:50:31 +0200

    Fixed DurationProcessor for formatting days

---
+++ pretroniclibraries-utility/src/main/java/net/pretronic/libraries/utility/duration/FixedChronoUnit.java
@@ -0,0 +1,99 @@
+package net.pretronic.libraries.utility.duration;
+
+import java.time.Duration;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+
+public enum FixedChronoUnit implements TemporalUnit {
+
+    MONTHS("Months", Duration.ofSeconds(86400*30));
+
+    private final String name;
+    private final Duration duration;
+
+    FixedChronoUnit(String name, Duration estimatedDuration) {
+        this.name = name;
+        this.duration = estimatedDuration;
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Gets the estimated duration of this unit in the ISO calendar system.
+     * <p>
+     * All of the units in this class have an estimated duration.
+     * Days vary due to daylight saving time, while months have different lengths.
+     *
+     * @return the estimated duration of this unit, not null
+     */
+    @Override
+    public Duration getDuration() {
+        return duration;
+    }
+
+    /**
+     * Checks if the duration of the unit is an estimate.
+     * <p>
+     * All time units in this class are considered to be accurate, while all date
+     * units in this class are considered to be estimated.
+     * <p>
+     * This definition ignores leap seconds, but considers that Days vary due to
+     * daylight saving time and months have different lengths.
+     *
+     * @return true if the duration is estimated, false if accurate
+     */
+    @Override
+    public boolean isDurationEstimated() {
+        throw new UnsupportedOperationException();
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Checks if this unit is a date unit.
+     * <p>
+     * All units from days to eras inclusive are date-based.
+     * Time-based units and {@code FOREVER} return false.
+     *
+     * @return true if a date unit, false if a time unit
+     */
+    @Override
+    public boolean isDateBased() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Checks if this unit is a time unit.
+     * <p>
+     * All units from nanos to half-days inclusive are time-based.
+     * Date-based units and {@code FOREVER} return false.
+     *
+     * @return true if a time unit, false if a date unit
+     */
+    @Override
+    public boolean isTimeBased() {
+        throw new UnsupportedOperationException();
+    }
+
+    //-----------------------------------------------------------------------
+    @Override
+    public boolean isSupportedBy(Temporal temporal) {
+        return temporal.isSupported(this);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <R extends Temporal> R addTo(R temporal, long amount) {
+        return (R) temporal.plus(amount, this);
+    }
+
+    //-----------------------------------------------------------------------
+    @Override
+    public long between(Temporal temporal1Inclusive, Temporal temporal2Exclusive) {
+        return temporal1Inclusive.until(temporal2Exclusive, this);
+    }
+
+    //-----------------------------------------------------------------------
+    @Override
+    public String toString() {
+        return name;
+    }
+}
